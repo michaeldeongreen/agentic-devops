7eca451a7388a791b11d2ba5d627820d8c5bcfae
michaeldeongreen <michaeldeongreen@gmail.com>
2026-01-19 14:58:46 -0600

    Remove stale app insights output

--- infra/terraform/modules/app_service/outputs.tf
+++ infra/terraform/modules/app_service/outputs.tf
@@ -4,9 +4,3 @@ output "app_service_url" {
   description = "Default hostname for the Web App."
   value       = azurerm_linux_web_app.this.default_hostname
 }
-
-output "app_insights_connection_string" {
-  description = "Application Insights connection string."
-  value       = azurerm_application_insights.this.connection_string
-  sensitive   = true
-}
